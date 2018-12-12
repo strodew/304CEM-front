dc74173c7e3891805630eb6264048fdf8661137e
William Strode <william.strode@gmail.com>
2018-12-12 16:25:47 +0000

    axios post code for SlideBar.js

--- src/components/SlideBar/SlideBar.js
+++ src/components/SlideBar/SlideBar.js
@@ -1,19 +1,59 @@
-import React, { Component } from 'react'
-import './SlideBar.css'
-import { slide as Menu } from 'react-burger-menu'
+import React, { Component } from "react";
+import "./SlideBar.css";
+import { slide as Menu } from "react-burger-menu";
+import axios from "axios";
 
 export default class SlideBar extends Component {
-    render() {
-        return (
-            <Menu>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Search:
-                        <input type='text' name='searchBar' onChange={this.handleChange} />
-                    </label>
-                    <button type='submit'>Search</button>
-                </form>
-            </Menu>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTxt: ""
+    };
+  }
+
+  updateSearchTxt(value) {
+    this.setState({
+      searchTxt: value
+    });
+  }
+
+  async submit() {
+    var data = {
+      searchTxt: this.state.searchTxt
+    };
+    const formData = new formData();
+    const config = {
+      headers: {
+        "content-type": "multipart/form-data"
+      }
+    };
+    await axios.post("http://localhost:8080/whatever", formData, config);
+  }
+
+  render() {
+    return (
+      <Menu>
+        <form>
+          <label>
+            Search:
+            <input
+              type="text"
+              name="searchBar"
+              onBlur={e => {
+                this.updateSearchTxt(e.target.value);
+              }}
+            />
+          </label>
+          <button
+            type="submit"
+            onclick={() => {
+              this.submit();
+            }}
+          >
+            Search
+          </button>
+        </form>
+      </Menu>
+    );
+  }
 }
