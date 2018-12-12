d0e8c3d3e1f98f1d3c19110099b2f4ea0425ae1d
William Strode <william.strode@gmail.com>
2018-12-12 16:49:06 +0000

    updated header and file architecture for displayitem component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import './App.css'
 import SlideBar from './components/SlideBar/SlideBar'
+import Header from './components/Header/Header'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <SlideBar/>
-      </div>
-        
+      </div>        
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,14 @@
-import React, {Component} from 'react'
-import './Header.css'
+import React, { Component } from "react";
+import "./Header.css";
 
 class Header extends Component {
+  handleSearchSubmit(event) {
+    event.preventDefault();
+    console.log("search button clicked");
+  }
 
-    handleSearchSubmit(event){
-        event.preventDefault()
-        console.log('search button clicked')
-    }
-
-    render() {
-        return (
-            <div className='header'>
-                <img src='' alt='React logo'/><a href='#default' className='logo'> React App Header</a>
-                <div className='header-right'>
-                    <div className='search-container'>
-                        <form action = ''>
-                            <input type='text' placeholder='Search...' name='txtSearch' onChange={this.handleInputChange}/>
-                            <button type='submit' onClick={this.handleSearchSubmit}>Search</button>
-                        </form>
-                     </div>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return <div className="header" />;
+  }
 }
-export default Header
+export default Header;
